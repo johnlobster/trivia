82d91aacfcb50e0b5129c467ccb943d06f01add3
John Webster <JohnLobster@comcast.net>
2019-04-08 11:54:45 -0700

    stepping between screens working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,10 +49,69 @@ function updateCard ( cardData) {
 
 }
 
+function nextQuestion () {
+    console.log("show next question card " + cardNumber);
+    answerSelected = "";
+    updateCard( questionCards[cardNumber]);
+    $("#startScreen").hide();
+    $("#answerScreen").hide();
+    $("#gameScreen").show();
+
+    localTimeout = setTimeout( resultScreen, 5000);
+
+
+}
+
+// game has finished, this shows stats and button for next game
+function summaryScreen() {
+
+    $("#startScreen").hide();
+    $("#answerScreen").hide();
+    $("#gameScreen").hide();
+    $("#summaryScreen").show();
+}
 function resultScreen (){
     console.log("show result screen");
+    // update results in answer screen
+
+    $("#resultQuestion").text("Question was: " +  questionCards[cardNumber].question);
+    $("#result").text("Your answer was: " + questionCards[cardNumber][answerSelected]);
+    var c = questionCards[cardNumber].correctAnswer;
+    $("#resultAnswer").text("Correct answer was: " + questionCards[cardNumber][c]);
+    if ( answerSelected === "") {
+        // timed out
+        console.log("timed out");
+        $("#answerBanner").text("You waited too long");
+        questionsWrong ++;
+    }
+    else if ( answerSelected === questionCards[cardNumber].correctAnswer ){
+        // correct answer
+        console.log( "correct");
+        $("#answerBanner").text("Your answer was correct !");
+        questionsRight ++;
+    }
+    else {
+        // wrong answer
+        console.log("incorrect");
+        $("#answerBanner").text("Your answer was wrong");
+        questionsWrong ++;
+    }
     $("#gameScreen").hide();
     $("#answerScreen").show();
+
+    // next question
+    cardNumber++;
+    if( cardNumber === questionCards.length) {
+        // seen all the questions so finish
+        console.log("All questions answered");
+        clearTimeout( localTimeout);
+        localTimeout = setTimeout( summaryScreen, 3000);
+    }
+    else {
+        // wait 3 seconds for user to see answers
+        clearTimeout( localTimeout);
+        localTimeout = setTimeout( nextQuestion, 3000);
+    } 
 }
 
 // set up events on answer buttons
@@ -62,12 +121,7 @@ $(".answerButton").on("click", function() {
     // Stop counting time when user selects an answer
     clearTimeout( localTimeout);
     resultScreen();
-    if( cardNumber === cardNumber.length) {
-        // seen all the questions so finish
-    }
-    localTimeout = setTimeout( function(){
-        resultScreen();
-    }, 5000);
+    
 });
 
 $(".startButtonClass").on("click", function() {
@@ -78,6 +132,7 @@ $(".startButtonClass").on("click", function() {
     answerSelected = "";
     updateCard( questionCards[cardNumber]);
     $("#startScreen").hide();
+    $("#summaryScreen").hide();
     $("#gameScreen").show();
 
     localTimeout = setTimeout( function(){
